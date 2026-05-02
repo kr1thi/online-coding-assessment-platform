0cd75e53fd398905804ad618fe4ae17a48d4d32c
Kiruthika J <145208691+kr1thi@users.noreply.github.com>
2026-05-02 13:43:26 +0530

    Add files via upload

---
+++ FamehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famehub.famehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
