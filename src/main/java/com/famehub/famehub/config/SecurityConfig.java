25d6e51dae5ac961fe182605b3226715788ed5fa
Kiruthika J <145208691+kr1thi@users.noreply.github.com>
2026-05-23 12:42:29 +0530

    Update SecurityConfig.java

--- src/main/java/com/famehub/famehub/config/SecurityConfig.java
+++ src/main/java/com/famehub/famehub/config/SecurityConfig.java
@@ -99,7 +99,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+    configuration.setAllowedOrigins(Arrays.asList(
+        "http://localhost:3000",
+        "https://online-coding-assessment-platform.vercel.app"
+    ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin"));
         configuration.setAllowCredentials(true);
