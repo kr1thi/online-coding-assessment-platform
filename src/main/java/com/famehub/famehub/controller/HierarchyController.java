a361376485a2cb372691022d095c05f13c62f289
Kiruthika J <145208691+kr1thi@users.noreply.github.com>
2026-05-23 13:13:36 +0530

    Update HierarchyController.java

--- src/main/java/com/famehub/famehub/controller/HierarchyController.java
+++ src/main/java/com/famehub/famehub/controller/HierarchyController.java
@@ -11,7 +11,10 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/hierarchy")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "https://online-coding-assessment-platform.vercel.app"
+})
 public class HierarchyController {
 
     @Autowired private InstitutionRepository instRepo;
