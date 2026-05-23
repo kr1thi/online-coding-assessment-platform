bbbbf299fd7b221235705758bcf308259b3b09e9
Kiruthika J <145208691+kr1thi@users.noreply.github.com>
2026-05-23 13:13:23 +0530

    Update InstitutionController.java

--- src/main/java/com/famehub/famehub/controller/InstitutionController.java
+++ src/main/java/com/famehub/famehub/controller/InstitutionController.java
@@ -10,7 +10,10 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/hierarchy/institutions")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "https://online-coding-assessment-platform.vercel.app"
+})
 public class InstitutionController {
 
     @Autowired 
