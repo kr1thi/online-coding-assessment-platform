b25654356c95abbbc16fde57b56d8c5b0a50bf12
Kiruthika J <145208691+kr1thi@users.noreply.github.com>
2026-05-25 13:54:57 +0530

    Update AddProblem.js

--- src/pages/AddProblem.js
+++ src/pages/AddProblem.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
 import axios from "axios";
 
-const API_BASE = "http://localhost:8082/api";
+const API_BASE = "https://online-coding-assessment-platform-production.up.railway.app/api";
 const styles = {
   cardStyle: {
     background: "#ffffff", // White background
